e3a3292bf686da1439fc67c9d949918e20a454d8
Thomas Morris <morenaruto007@gmail.com>
2024-07-07 17:28:18 -0500

    Added all book reviews to page

--- src/components/BookReviews.svelte
+++ src/components/BookReviews.svelte
@@ -56,10 +56,71 @@
         { title: "Forget 'Having It All': How America Messed Up Motherhood--and How to Fix It", author: "Amy Westervelt", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/ba40bc16-5835-4a88-ba52-a9051aef1438" },
         { title: "How to Change Minds The Art of Influence Without Manipulation", author: "Rob Jolles", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/d012ce1b-06ec-4b69-a7f8-d8ab0bb2cf31" },
         { title: "How to Have Impossible Conversations", author: "Peter Boghossian and James Lindsay", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/e2336728-2f05-42a9-8244-a4123cd2f232" },
+        { title: "Everyone Lies", author: "Seth Stephens-Davidowitz", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/5ca4fbd7-f0a2-44da-bff1-38e3844ee2fe" },
+        { title: "When Breath Becomes Air", author: "Paul Kalanithi", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/04aa0919-6c77-4932-95db-e6d966688b5c" },
+        { title: "The Madness of Crowds", author: "Douglass Murray", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/79768804-0343-47e2-b7c0-25527298bd59" },
+        { title: "The Tipping Point", author: "Malcolm Gladwell", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/2674b0f5-7616-4c0b-b54d-45e9961fc6bd" },
+        { title: "The Lucifer Effect", author: "Philip Zimbardo", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/d854fd57-8026-4059-af01-a57f43ddf1e2" },
+        { title: "Dare to Lead", author: "Brene Brown", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/4b65e639-22c2-46d6-8eed-e5adf0d72881" },
+        { title: "Start with Why", author: "Simon Sinek", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/f3408589-e687-4fbb-a853-be8f6164fe14" },
+        { title: "Human Diversity: The Biology of Gender, Race, and Class", author: "Charles Murray", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/95948f81-bd2a-436a-934c-b305a5625530" },
+        { title: "Understanding the Dark Side of Human Nature", author: "Daniel Breyer", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/6c8a8dae-1500-480a-b9c4-ec2ebaf83af2" },
+        { title: "Letter to a Christian Nation", author: "Sam Harris", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/a7baa741-1dd4-4688-bbcf-ef0a3b97e5b6" },
+        { title: "How will you measure your life?", author: "Clayton M. Christensen, James Allworth, and Karen Dillon", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/a8d6c580-e5a6-48a1-99b4-0012a0e62a2a" },
+        { title: "Twelve Pillars", author: "Jim Rohn and Chris Widener", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/de2c5c3d-682e-4da1-a96e-c5e4845f3fb4" },
+        { title: "Fantasyland: How American Went Haywire, A 500-Year History", author: "Kurt Andersen", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/1d975a4e-f611-42fc-a742-c5650c39f2b6" },
+        { title: "Team Human | The Social Dilemma", author: "Douglas Rushkoff | Tristian Harris", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/a5abd74c-840b-4641-87d0-c4eca5c43031" },
+        { title: "Loneliness: Human Nature and the Need for Social Connection", author: "John T. Cacioppo & William Patrick", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/b0603505-83aa-4d40-b4de-29364fbbcd88" },
+        { title: "Democracy At Work: A Cure For Capitalism", author: "Richard D. Wolff", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/a996ea79-0df3-4cc6-b5a1-220de1f81cad" },
+        { title: "The Meritocracy Trap", author: "Daniel Markovits", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/1acecc01-5f09-4443-a0c3-024e95668b2c" },
+        { title: "Creativity Inc.: Overcoming the Unseen Forces That Stand in the Way of True Inspiration", author: "Ed Catmull", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/5f69791f-2b3b-4acc-ba64-08d7b9246a86" },
+        { title: "Blueprint: The Evolutionary Origins of a Good Society", author: "Nicholas A Christakis", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/59377ba5-acfd-4c8f-8c00-490254ddfc44" },
+        { title: "The Four Horsemen: The Conversation that Sparked an Atheist Revolution", author: "Stephen Fry", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/64d234ce-c67b-4aa6-b801-d6d0881bac5f" },
+        { title: "The Precipice: Existential Risk and the Future of Humanity | Engineering The Apocalypse", author: "Toby Ord | Rob Reid", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/ead10920-2950-40e3-b752-282c4e363803" },
+        { title: "Management: Tasks, Responsibilities, Practices", author: "Peter F. Drucker", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/7be83522-9eec-4c96-9b9b-9199b7c4bc0a" },
+        { title: "The Phoenix Project", author: "Gene Kim, Kevin Behr, and George Spafford", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/4bb69f6b-fbc2-4de6-b9bc-2b209426ca2c" },
+        { title: "Conscious: A Brief Guide to the Fundamental Mystery of the Mind", author: "Annaka Harris", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/196bf5a9-bdeb-41de-a6b4-bdf2e0813c77" },
+        { title: "How to Avoid A Climate Disaster: The Solution we have to and the Breakthroughs We Need", author: "Bill Gates", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/26c3be10-cba4-4d3a-bf48-3d6a6a2628b7" },
+        { title: "Human Compatible: Artificial Intelligence And The Problem of Control", author: "Stuart Russell", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/436ed520-169d-4251-b59b-e479e13812b8" },
+        { title: "Evicted: Poverty and Profit in the American City", author: "Matthew Desmond", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/8f86e6d1-96b4-4ec1-9620-69c3854f4e95" },
+        { title: "The Master and His Emissary: The Divided Brain and the Making of the Western World", author: "Iain McGilchrist", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/a6dbaa47-d406-4942-a974-c7efb5688830" },
+        { title: "Invisible Women: Data Bias in a World Designed for Men", author: "Caroline Criado Perez", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/dcdbfa83-dad2-49b5-a2e6-0ff898d8bddb" },
+        { title: "Fooled by Randomness: The Hidden Role of Chance in Life and in the Markets", author: "Nassim Nicholas Taleb", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/696436f5-9c1e-4dee-910a-432e326ae6cd" },
+        { title: "Nudge: The Final Edition", author: "Richard H. Thaler and Cass R. Sunstein", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/939196bf-af54-4776-9b53-23a58b70e17e" },
+        { title: "Never Lose a Customer Again: Turn Any Sale Into LifeLong Loyalty in 100 Days", author: "Joey Coleman", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/d051ed6c-2b3b-4230-b793-20128c91fb75" },
+        { title: "Superbugs: The Race to Stop an Epidemic", author: "Matt McCarthy MD", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/ae0ebcd0-f828-4c64-ac0f-5c8efe9d3129" },
+        { title: "Crucial Conversations: Tools for Talking When Stakes are High", author: "Joseph Grenny", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/217cd67d-1dcf-4260-9a75-58e76a7b4f6e" },
+        { title: "Transcend: The New Science of Self-Actualization", author: "Scott Barry Kaufman", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/0f67d64b-e6d8-4dbf-94b5-b29407cc8ad1" },
+        { title: "Bullshit Jobs", author: "David Graeber", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/f919f2ee-d4c3-4033-9d88-7ef2543bf824" },
+        { title: "David and Goliath", author: "Malcolm Gladwell", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/3bd71ce7-f00e-4669-8979-bd5e39a20672" },
+        { title: "Why We Sleep: Unlocking the Power of Sleep and Dreams", author: "Matthew Walker, PhD", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/f90a4462-7a82-4e83-b943-ec2afe2f6c8d" },
+        { title: "Woke Racism: 'How a New Religion Has Betrayed Black America'", author: "John McWhorter", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/a69805d6-c729-47df-b2b1-41562677c912" },
+        { title: "The Code Breaker: Jennifer Doudna, Gene Editing, and the Future of the Human Race", author: "Walter Isaacson", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/d860fbb2-a3e8-4b06-aa58-f02eee548357" },
+        { title: "Why We’re Polarized", author: "Ezra Klein", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/2aea96c1-a58d-461b-8157-787e3656b64a" },
+        { title: "Startup CEO: A Field Guild to Scaling Up Your Business", author: "Matt Blumberg", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/0e03330a-36a5-4e99-8381-d00fffc25a80" },
+        { title: "When Men Behave Badly: The Hidden Roots of Sexual Deception, Harassment, and Assault", author: "David M. Buss", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/51f33f9e-51b5-4d26-bd92-190e4b4f6c37" },
+        { title: "A Thousand Brains: A New Theory of Intelligence", author: "Jeff Hawkins", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/7bd8efe5-8f03-4d8f-adca-202234b51bab" },
+        { title: "A Beginner’s Guide to the End: Practical Advice for Living Life and Facing Death", author: "BJ Miller and Shoshana Berger", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/e5386ade-1a18-4548-b5ed-70d3fe4564f6" },
+        { title: "Infectious Greed: How Deceit and Risk Corrupted the Financial Markets", author: "Frank Partnoy", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/5622aa2a-574c-4191-9e8e-f2a2448e15cf" },
+        { title: "Smarter Tomorrow: How 15 Minutes of Neurohacking a Day Can Help You Work Better, Think Faster, and Get More Done", author: "Elizabeth R. Ricker", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/8b2018b3-3528-4911-b306-2722fe091d83" },
+        { title: "The Age of Surveillance Capitalism: The Fight for a Human Future At the New Frontier of Power", author: "Shoshana Zuboff", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/8fe493c3-f8bd-43ec-9eca-969c20926b15" },
+        { title: "Men Without Work: Post-Pandemic Edition", author: "Nicholas Eberstadt", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/f9f7d582-9e1b-426e-956a-676309296c0a" },
+        { title: "Sam Harris: Making Sense Podcast Episode #335", author: "Sam Harris", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/55e75a79-6575-43f0-8842-5b3e5785bd0b" },
+        { title: "Bad Blood: Secrets and Lies in a Silicon Valley Startup", author: "John Carreyrou", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/1fc0517b-688b-40da-8fff-5c398786ae0b" },
+        { title: "Manufacturing Consent: The Political Economy of the Mass Media", author: "Edwards S. Herman & Noam Chomsky", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/a083e258-236a-48d6-ab32-e31e0439cf7d" },
+        { title: "Accelerate: Building and Scaling High Performing Technology Organizations", author: "Nicole Forsgen, Jez Humble, and Gene Kim", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/5099438f-a20e-4bea-981a-a42d50e5698b" },
+        { title: "Good Economics for Hard Times", author: "Abhijit V. Banerjee and Esther Duflo", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/c7343f82-d50c-4f6b-bfbe-a53add72805e" },
+        { title: "Useful Delusions: The Power & Paradox of the Self-Deceiving Brain", author: "Shankar Vedantam & Bill Mesler", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/847bb31e-fb5d-4bc9-9942-8749634ba148" },
+        { title: "Editing Humanity: The Crispr Revolution and The New Era of Genome Editing", author: "Kevin Davies", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/8f2d49bc-ad60-4442-90f4-90d4e4310120" },
+        { title: "Venture Deals: Be Smarter Than Your Lawyer and Venture Capitalist (4th Edition)", author: "Brad Feld & Jason Mendelson", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/f00f8350-de71-4cc1-8dcb-93a0a4efd6b3" },
+        { title: "Strangers in Their Own Land", author: "Arlie Russell Hochschild", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/735a458c-5f39-455c-9fdd-7b50d2bad12d" },
+        { title: "The Abandonment of the Jews: America and the Holocaust, 1941-1945", author: "David S. Wyman", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/b083dae0-b57b-41fc-8360-a7aaa00d86f7" },
+        { title: "Our Revolution", author: "Bernie Sanders", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/bbc8c48a-f354-4ba6-94fa-be024eefe816" },
+        { title: "Managing Humans: Biting and Humorous Tales of Software Engineering Manager", author: "Michael Lopp", image: "https://github.com/MoreNaruto/personal-website/assets/2292947/d141ada3-cf17-4d72-a60d-1cf2b82edb26" },
     ];
 
     let currentPage = 1;
-    const booksPerPage = 12;
+    const booksPerPage = 16;
     let paginatedBooks = [];
 
     function paginateBooks() {
@@ -102,8 +163,9 @@
     }
 </script>
 
-<main class="p-4 bg-slate-200 pb-16">
+<main class="p-4 bg-slate-200 {selectedBookIndex === null ? 'min-h-[80vw]' : ''}">
     {#if selectedBookIndex === null}
+        <h1 class="text-center">My Personal Reviews</h1>
         <div class="mb-4">
             <div class="relative max-w-4xl mx-auto">
                 <div class="grid xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-center" in:fade>
@@ -112,7 +174,6 @@
                             <div class="w-full h-full flex justify-center items-center">
                                 <img src={image} alt={title} class="w-40 h-40 object-cover rounded-lg"/>
                             </div>
-                            <h3 class="mt-2 text-lg font-bold">"{title}" By {author}</h3>
                         </div>
                     {/each}
                 </div>
@@ -147,6 +208,9 @@
 
 
 <style>
+    h1 {
+        @apply text-3xl;
+    }
     .prose h1 {
         @apply text-2xl;
     }
